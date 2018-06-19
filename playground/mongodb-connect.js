6afd036354a37fb6a9b76192ecba02fc9545b5e2
Shanawar Hasnain <shanawar@curefit.com>
2018-06-20 02:12:43 +0530

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,61 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Do Something',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // })
+
+    // db.collection('Users').insertOne({
+    //     name: 'Shanawar',
+    //     age: 26,
+    //     location: 'Bangalore'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+    // })
+
+    db.close();
+});
+
+/*
+    code change required while using v3 of mongodb 
+*/
+/*
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').insertOne({
+        text: 'Do Something',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    })
+
+    client.close();
+});
+*/
---
+++ playground/mongodb-find.js
@@ -0,0 +1,53 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({_id: new ObjectID('5b28fcae1364d92d686ed41e')}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to insert todo', err);
+    // });
+
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
+    }, (err) => {
+        console.log('Unable to insert todo', err);
+    });
+
+    // db.close();
+});
+
+/*
+    code change required while using v3 of mongodb 
+*/
+/*
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').insertOne({
+        text: 'Do Something',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    })
+
+    client.close();
+});
+*/
